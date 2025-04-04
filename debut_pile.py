ffb8c1d43288cceca37236a6fe3660c0ca61d0e8
LucileL <lucile.guigui@gmail.com>
2025-04-04 13:39:12 +0200

    V2

---
+++ debut_pile.py
@@ -0,0 +1,14 @@
+class pile()
+    def __init__(self):
+        self.souvenirs=[]
+    
+    def ajout_souvenir(souvenir):
+        self.souvenirs.append(souvenir)
+    
+    def depiler():
+        if self.souvenirs:
+            self.souvenirs.pop()
+
+
+print("zeub")
+
