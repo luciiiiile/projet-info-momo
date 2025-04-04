e86c3120191870ebf4c660b956185545093c1275
LucileL <lucile.guigui@gmail.com>
2025-04-04 13:35:08 +0200

    v1

---
+++ debut_arbre.py
@@ -0,0 +1,48 @@
+import pygame  # Importer pygame pour gérer les sons
+import random
+
+
+class Noeud:
+    def __init__(self,valeur):
+        self.valeur = valeur
+        self.enfants = []
+
+    def ajouter_choix(self, choix):
+        self.enfants.append(choix)
+
+    def afficher(self):
+        print("User : quel est la personne que vous voulez voir ?")
+        if self.enfants:
+            print("les choix sont les suivants :")
+            for enfant in (self.enfants):
+                print(f"{self.enfants.index(enfant) + 1} : {enfant.valeur}")
+            rep = int(input("Entrez le numero de la personne que vous voulez rencontrer :"))
+            self.enfants[rep - 1].afficher()
+        else:
+            print("bruh")
+
+racine=Noeud("Christopher")
+print("bruh")
+
+enfant1 = Noeud("facteur")
+enfant2 = Noeud("boulanger")
+enfant3 = Noeud("infirmiere")
+
+
+
+petitenfant1 = Noeud("opticien")
+petitenfant2 = Noeud("caissiere")
+petitenfant3 = Noeud("vendeur_citron")
+
+arrierepetitenfant1 = Noeud("leandre")
+arrierepetitenfant2 = Noeud("samuel")
+
+
+racine.enfants = [enfant1 , enfant2, enfant3]
+
+enfant2.enfants = [petitenfant1, petitenfant2, petitenfant3]
+petitenfant2.enfants = [arrierepetitenfant1, arrierepetitenfant2]
+
+#input("*vous avez décidé d'aller voir les différentes personne de votre village afin dans apprendre plus sur ce qui c'est réellement passer cette soirée là*")
+
+racine.afficher()
---
+++ debut_code.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 31 12:36:59 2025
+
+@author: gerem
+"""
+import time,os
+import pygame  
+
+pygame.mixer.init()
+
+son_sonnerie = pygame.mixer.Sound("sons/sonnerie_telephone.mp3")
+
+son_raccrocher = pygame.mixer.Sound("sons/raccrocher.mp3")
+
+#fontion qui lance les sons du jeu 
+def jouer_son(son):
+    son.play()
+    time.sleep(son.get_length())
+
+
+
+def afficher_titre():
+    print("/////-----MNEMOSYS-----/////")
+    print("")
+    print("")
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def opening():
+    clear()
+    print("Un jeu fait par Lucile Lagier et Gérémy Gamarra")
+    print("")
+    print("Inspiré de Memento de Christopher Nolan")
+    print("")
+    print("")
+    afficher_titre()
+    username = str(input("Saisissez votre pseudo : ")).strip()
+    print(f"Bienvenue {username}...")
+    time.sleep(1)
+    input("Taper ENTRER pour commencer à jouer.")      
+    print("Le jeu commence...")
+    time.sleep(1)
+    introduction(username)
+
+def introduction(username):
+    clear()
+    afficher_titre()
+    input("*Vous venez de vous reveiller*")
+    input("*Vous vous lever et vous allez pour vous habiller*") 
+    input("*Le telephone sonne*")
+    print("Telephone : << Dring Dring >>")
+    jouer_son(son_sonnerie)
+    input(f"{username} : << Oui, Allo ? >>")
+    input("Police : << Bonjour, c'est la police au téléphone. >>")
+    input("Police : << Nous sommes dans le regret de vous annoncer que nous suspendons momentanément l'enquête sur la mort de votre frère. >>")
+    input(f"{username} : << Mais !! Qu'est ce que vous racontez ? >>")
+    input("Police : << On comprend que cela est difficile à accepter, cependant nous sommes débordés. >>")
+    input("Police : << Désolé. >>")
+    input(f"{username} : << Attendez Monsieur !... >>")
+    print("Telephone : << Tut tut >>")
+    jouer_son(son_raccrocher)
+    input(f"{username} : << Mon frère est mort ? Qu'est ce qui me raconte celui là ? >>")
+    input("*Vous commencez à paniquer*")
+    input(f"{username} : << Je suis quasiment sur qu'on s'est vu hier. >>")
+    input(f"{username} : << Enfin... J'ai quelques flash-backs mais cela est assez flou. >>")
+    input(f"{username} : << C'est comme si mes souvenirs étaient enfouis dans ma mémoire. >>")
+    input(f"{username} : << Mais je suis incapable de me souvenir de ce qui s'est passé. >>")
+    input("*Vous vous dites que vous allez surement trouver de l'aide dehors, en demandant aux autres villageois de Mnémosys*")
+     
+opening()
